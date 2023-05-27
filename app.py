ebaf44a54eafeeacaf5886844cf2da61a131cd8d
yagarwal0 <agarwal.yash800@gmail.com>
2023-05-27 19:48:17 +0530

    math function

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return "HELLO World"
 
+
+@app.route("/math")
+def maths_func():
+    return "Aoooo bacho math sikhata hu tumko"
+
 if __name__=="__main__":
     app.run("0.0.0.0")
